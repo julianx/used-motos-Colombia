3ddd39cd1fc821d8734cbea0cf6d1439615344c5
Julian Urán <julianx@gmail.com>
2016-05-29 13:29:45 -0500

    Fixed maker and bike looping issue.

--- tuMotoWholeSiteScrapper.py
+++ tuMotoWholeSiteScrapper.py
@@ -9,14 +9,14 @@ browser.open('http://listado.tumoto.com.co/motos/')
 makers = browser.select('#id_9991763-AMCO_1763_2 .qcat-truncate')
 for i in range(0, len(makers)):
     browser.follow_link(makers[i])
-    next_page = browser.find_all('a', class_="prefetch")
 
-    while len(next_page) > 0:
-        bikes = browser.select('.list-view-item-title a')
-        bikes_index = range(0, len(bikes))
+    bikes = browser.select('.list-view-item-title a')
+    bikes_index = range(0, len(bikes))
+
+    while len(bikes_index) > 0:
+        next_page = browser.find_all('a', class_="prefetch")
         for j in bikes_index:
             browser.follow_link(bikes[j])
-
             techs = browser.find_all(class_='tit')
             for tech in techs:
                 if tech.text == 'Cilindrada (cc):':
@@ -39,10 +39,14 @@ for i in range(0, len(makers)):
             kms = browser.find_all(class_="atrDest")[1].text.replace('|', '').replace('km', '').strip()
             price = browser.find(class_='ch-price').text.replace('\t', '').replace('$', '').replace('.', '')
 
-            with open('motos.csv', 'a', newline='') as csvfile:
+            with open('all_motos.csv', 'a', newline='') as csvfile:
                 csv_writer = csv.writer(csvfile, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
                 csv_writer.writerow([model, kms, img, title, link, price, year, maker])
             print("{};{};{};{};{};{};{};{}".format(model, kms, img, title, link, price, year, maker))
 
-        browser.follow_link(next_page[0])
-        next_page = browser.find_all('a', class_="prefetch")
+        if len(next_page) > 0:
+            browser.follow_link(next_page[0])
+            bikes = browser.select('.list-view-item-title a')
+            bikes_index = range(0, len(bikes))
+        else:
+            bikes_index = []
