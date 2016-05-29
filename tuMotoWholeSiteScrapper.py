b4ccdea7872e20d78310d407e455c0a33eed8724
Julian Urán <julianx@gmail.com>
2016-05-29 15:36:08 -0500

    Changed CSV wiring logic.

--- tuMotoWholeSiteScrapper.py
+++ tuMotoWholeSiteScrapper.py
@@ -1,11 +1,9 @@
 from robobrowser import RoboBrowser
 import csv
 
-# Browse to a page with checkbox inputs
+results = []
 browser = RoboBrowser()
-# browser = RoboBrowser(history=True)
 browser.open('http://listado.tumoto.com.co/motos/')
-
 makers = browser.select('#id_9991763-AMCO_1763_2 .qcat-truncate')
 for i in range(0, len(makers)):
     browser.follow_link(makers[i])
@@ -21,28 +19,27 @@ for i in range(0, len(makers)):
             for tech in techs:
                 if tech.text == 'Cilindrada (cc):':
                     cc = tech.next.next.next.text
-                elif tech.text == 'Recorrido:':
-                    mileage = tech.next.next.next.text
+                    continue
                 elif tech.text == 'Modelo:':
                     model = tech.next.next.next.text
-                elif tech.text == 'Color:':
-                    color = tech.next.next.next.text
+                    continue
                 elif tech.text == 'Marca:':
                     maker = tech.next.next.next.text
+                    continue
                 elif tech.text == 'Tipo:':
                     type = tech.next.next.next.text
+                    continue
             link = browser.url
             title = browser.find('h1').text
             location = browser.find(class_='ubic').text
             year = browser.find_all(class_="atrDest")[0].text
-            img = browser.find(class_="src-delay-initiation").attrs['data-src-original']
+            img = browser.find(class_="src-delay-initiation")
+            img = '' if img is None else img.attrs['data-src-original']
             kms = browser.find_all(class_="atrDest")[1].text.replace('|', '').replace('km', '').strip()
             price = browser.find(class_='ch-price').text.replace('\t', '').replace('$', '').replace('.', '')
 
-            with open('all_motos.csv', 'a', newline='') as csvfile:
-                csv_writer = csv.writer(csvfile, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                csv_writer.writerow([model, kms, img, title, link, price, year, maker])
-            print("{};{};{};{};{};{};{};{}".format(model, kms, img, title, link, price, year, maker))
+            results.append([model, kms, img, title, link, price, year, maker, cc, type])
+            print("{};{};{};{};{};{};{};{};{};{}".format(model, kms, img, title, link, price, year, maker, cc, type))
 
         if len(next_page) > 0:
             browser.follow_link(next_page[0])
@@ -50,3 +47,9 @@ for i in range(0, len(makers)):
             bikes_index = range(0, len(bikes))
         else:
             bikes_index = []
+
+with open('all_motos.csv', 'a', newline='') as csvfile:
+    csv_writer = csv.writer(csvfile, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    csv_writer.writerow(['model', 'kms', 'img', 'title', 'link', 'price', 'year', 'maker', 'cc', 'type'])
+    for result in results:
+        csv_writer.writerow(result)
